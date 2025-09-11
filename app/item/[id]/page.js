0ecb0b90e1bea94622578bd0691c43d423dd4ae0
Masaki Suzuki <masakisuzuki.shkr@gmail.com>
2025-09-12 05:51:10 +0900

    APIをfetchで表示する方法の学習

--- app/item/[id]/page.js
+++ app/item/[id]/page.js
@@ -1,13 +1,22 @@
 export const generateMetadata = async ({ params }) => {
     const { id } = await params
+    const response = await fetch(`http://localhost:3000/api?id=${id}`)
+    const item = await response.json()
     return {
-        title: `Item${id}`,
+        title: item.name,
     }
 }
 
 const Item = async ({ params }) => {
     const { id } = await params;
-    return <div>Item{id}ページ</div>
+    const response = await fetch(`http://localhost:3000/api?id=${id}`)
+    const item = await response.json()
+    return (
+        <>
+            <h2>{item.name}</h2>
+            <p>価格： {item.price}円</p>
+        </>
+    )
 }
 
 export default Item
